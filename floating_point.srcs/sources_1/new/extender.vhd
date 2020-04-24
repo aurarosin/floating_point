f842790dcd48052246062e7d76ba281718776ab5
Rosaura HG <rosshg.nw@gmail.com>
2020-04-23 23:31:53 -0500

    extender

--- floating_point.srcs/sources_1/new/extender.vhd
+++ floating_point.srcs/sources_1/new/extender.vhd
@@ -1,44 +1,16 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 23.04.2020 23:03:48
--- Design Name: 
--- Module Name: extender - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity extender is
     Port ( i_data : in STD_LOGIC_VECTOR (22 downto 0);
-           o_data : out STD_LOGIC_VECTOR (24 downto 0));
+           o_data : out STD_LOGIC_VECTOR (23 downto 0));
 end extender;
 
 architecture Behavioral of extender is
 
 begin
 
+o_data <= '1'&i_data;
 
 end Behavioral;
