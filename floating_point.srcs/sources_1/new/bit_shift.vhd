e8cbef7cacf693e96256a67c0c404d97c1004ba8
Rosaura HG <rosshg.nw@gmail.com>
2020-04-24 00:03:34 -0500

    bit shift

--- floating_point.srcs/sources_1/new/bit_shift.vhd
+++ floating_point.srcs/sources_1/new/bit_shift.vhd
@@ -1,35 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 23.04.2020 23:03:48
--- Design Name: 
--- Module Name: bit_shift - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity bit_shift is
     Port ( i_data : in STD_LOGIC_VECTOR (23 downto 0);
@@ -39,7 +12,11 @@ end bit_shift;
 
 architecture Behavioral of bit_shift is
 
+--variable tmp : std_logic_vector(15 downto 0);
+
 begin
+    
+    o_data <= STD_LOGIC_VECTOR(unsigned (i_data) srl to_integer(unsigned(n)));
 
 
 end Behavioral;
